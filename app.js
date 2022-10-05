7ecd4a1358a778ef3ecb73c6f3211c185bf3e043
eortiz65 <eortiz599@gmail.com>
2022-10-05 09:41:54 -0400

    replicated my tic tac toe to have elements for calibrating where i want my elements

---
+++ app.js
@@ -0,0 +1,10 @@
+////////////////////////////////
+// Global Variables Here
+let player
+let playerTurn = -1
+let board
+let winner = false
+let count = 0
+
+const message = document.getElementById('message')
+const playAgain = document.getElementById('play')
