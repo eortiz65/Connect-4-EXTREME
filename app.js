b60098685bc9cd97485dfde53a480d362b7b8945
eortiz65 <eortiz599@gmail.com>
2022-10-06 17:49:29 -0400

    Removing some duplicate and unwanted code

--- app.js
+++ app.js
@@ -10,10 +10,35 @@ let rows = 6
 let columnIndex
 
 const startGame = document.getElementById('play')
-// window.onload = () => {
-//   gameStart()
-// }
 
+gameStart = () => {
+  board = []
+  columnIndex = [
+    rows - 1,
+    rows - 1,
+    rows - 1,
+    rows - 1,
+    rows - 1,
+    rows - 1,
+    rows - 1
+  ]
+  startGame.style.visibility = `hidden`
+
+  for (let r = 0; r < rows; r++) {
+    let row = []
+    for (let c = 0; c < columns; c++) {
+      // Js operator to assign value of ` ` so we can further use that as an indicator to be able to add pieces.
+      row.push(' ')
+      // creates the board in HTML to avoid making all 42 tiles individually, it also adds the class and ID for each tile on board, starting on tile 0-0 on the top left of board and ending 5-6 on bottom right and the event listener for the tile to be clicked
+      let tile = document.createElement('div')
+      tile.id = r.toString() + `-` + c.toString()
+      tile.classList.add(`tile`)
+      tile.addEventListener(`click`, setPiece) //adds and event listener to each tile so when you click it indicates that column
+      document.getElementById(`board`).append(tile)
+    }
+    board.push(row)
+  }
+}
 setWinner = (r, c) => {
   let winner = document.getElementById('winner')
   if (board[r][c] === playerBlue) {
@@ -90,35 +115,6 @@ checkWinner = () => {
   }
 }
 
-gameStart = () => {
-  board = []
-  columnIndex = [
-    rows - 1,
-    rows - 1,
-    rows - 1,
-    rows - 1,
-    rows - 1,
-    rows - 1,
-    rows - 1
-  ]
-  startGame.style.visibility = `hidden`
-
-  for (let r = 0; r < rows; r++) {
-    let row = []
-    for (let c = 0; c < columns; c++) {
-      // Js operator to assign value of ` ` so we can further use that as an indicator to be able to add pieces.
-      row.push(' ')
-      // creates the board in HTML to avoid making all 42 tiles individually, it also adds the class and ID for each tile on board, starting on tile 0-0 on the top left of board and ending 5-6 on bottom right and the event listener for the tile to be clicked
-      let tile = document.createElement('div')
-      tile.id = r.toString() + `-` + c.toString()
-      tile.classList.add(`tile`)
-      tile.addEventListener(`click`, setPiece) //adds and event listener to each tile so when you click it indicates that column
-      document.getElementById(`board`).append(tile)
-    }
-    board.push(row)
-  }
-}
-
 setPiece = () => {
   //Checks if winner is true to avoid adding pieces if game is done
   if (winnerPlayer) {
@@ -126,11 +122,11 @@ setPiece = () => {
   }
 
   //get coords of that tile clicked
-  let coords = this.id
+  let coords = this.attributes.id.value
   console.log(coords)
 
-  // let r = parseInt(coords[0])
-  // let c = parseInt(coords[1])
+  let r = 2 //parseInt(coords[0])document.querySelector("#\\30 -0")document.querySelector("#\\30 -5")
+  let c = 2 //parseInt(coords[1])
 
   // Changes the value of r to the height of that column so it drops in the proper spot
   r = columnIndex[c]
@@ -150,7 +146,7 @@ setPiece = () => {
   }
 
   r -= 1 //update the row height for that column, signifying one more element inside
-  columnIndex[c] = r //update the array height
+  columnIndex[2] = r //update the array height
 
   checkWinner()
 }
